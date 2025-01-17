53bb4001fe7d8da78887aeb2a7429647e6aec441
O22415 <O22415@lab.aca>
2025-01-17 12:47:54 -0500

    a

---
+++ ejercicio2.ts
@@ -0,0 +1,23 @@
+interface TODO {
+    codigo: number;
+    nombreTarea: string;
+    estado: boolean;
+  }
+
+const listaTODOs : TODO[] = []
+
+
+export function agregarTODO( listTODOs){
+    return listTODOs.push(listaTODOs,listTODOs.boolean = false);
+
+}
+
+console.log();
+
+export function marcarTODO (listaTODOs: TODO[], y: number){
+
+}
+
+export function reporteTODOs (listaTODOs: TODO[]){
+
+}
